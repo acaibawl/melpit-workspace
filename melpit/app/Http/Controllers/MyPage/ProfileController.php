5dd767d6bc8d5c800d3c599ce614505ad2cd4aaa
acaiibawl <hagiya0719@gmail.com>
2022-01-13 08:32:32 +0000

    プロフィールの保存機能を実装

--- melpit/app/Http/Controllers/MyPage/ProfileController.php
+++ melpit/app/Http/Controllers/MyPage/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\MyPage;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Mypage\Profile\EditRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,5 +12,17 @@ class ProfileController extends Controller
     public function showProfileEditForm()
     {
         return view('mypage.profile_edit_form')->with('user', Auth::user());
+    }
+
+    public function editProfile(EditRequest $request)
+    {
+        /** @var \App\Models\User */
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        // 変数に値を詰めて直前の画面にリダイレクトする
+        return redirect()->back()->with('status', 'プロフィールを変更しました。');
     }
 }
