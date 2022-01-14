f39369c71296f89655768639d4850097824ada40
acaiibawl <hagiya0719@gmail.com>
2022-01-14 06:26:54 +0000

    購入した商品一覧画面作成

--- melpit/app/Models/Item.php
+++ melpit/app/Models/Item.php
@@ -12,6 +12,11 @@ class Item extends Model
     // 購入済み
     const STATE_BOUGHT = 'bought';
 
+    // Eloquent Modelのカラムの値を取り出す際に、データ型を変換できる。datetimeはCarbonクラスになる。
+    protected $casts = [
+        'bought_at' => 'datetime',
+    ];
+
     public function secondaryCategory()
     {
         return $this->belongsTo(SecondaryCategory::class);
